ccf1934fac1a65a0c036b17398b0bfedf962c90c
Kwasi Hickson <kwasi.hickson@gmail.com>
2022-04-01 15:19:15 -0400

    New Login

---
+++ src/app/auth-service.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { baseURL } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthServiceService {
+
+  constructor(private http:HttpClient) { } 
+  login(data: any):Observable<any>{
+      return this.http.post(`${baseURL}users/login`, data);
+    }
+  }
